5aeb8a807c033cc90917307aa134a2bf5f16fa5b
cklewar <cklewar@t-online.de>
2023-05-16 14:40:30 +0200

    0.11.21 v3

---
+++ firewall.tf
@@ -0,0 +1,36 @@
+resource "volterra_network_policy" "allow_all" {
+  name      = format("%s-allow-all-%s", var.project_prefix, var.project_suffix)
+  namespace = var.f5xc_namespace
+  endpoint {
+    any = true
+  }
+  rules {
+    egress_rules {
+      action = "ALLOW"
+      metadata {
+        name = "allow-egress-all"
+      }
+      any         = true
+      all_traffic = true
+    }
+    ingress_rules {
+      action = "ALLOW"
+      metadata {
+        name = "allow-ingress-all"
+      }
+      any         = true
+      all_traffic = true
+    }
+  }
+
+}
+
+resource "volterra_active_network_policies" "active" {
+  depends_on = [volterra_network_policy.allow_all]
+  namespace  = var.f5xc_namespace
+  policies {
+    name      = format("%s-allow-all-%s", var.project_prefix, var.project_suffix)
+    namespace = var.f5xc_namespace
+    tenant    = var.f5xc_tenant
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,8 @@
 output "mcn" {
   value = {
-    aws   = module.aws.f5xc_aws_tgw
-    gco   = module.gcp.gcp_vpc
-    azure = module.azure.vnet
+    aws      = module.aws.f5xc_aws_tgw
+    gco      = module.gcp.gcp_vpc
+    azure    = module.azure.vnet
+    firewall = volterra_active_network_policies.active
   }
 }
