41ddcc52943c21fa19264cfd7b033e73ebd6c873
cklewar <cklewar@t-online.de>
2023-11-07 17:37:32 +0100

    Launch 0.11.23

--- versions.tf
+++ versions.tf
@@ -1,28 +1,32 @@
 terraform {
   required_version = ">= 1.3.0"
+  cloud {
+    organization = "cklewar"
+    hostname     = "app.terraform.io"
 
+    workspaces {
+      name = "f5-xc-mcn-transit-module"
+    }
+  }
+  
   required_providers {
     volterra = {
-      source  = "volterraedge/volterra"
-      version = ">= 0.11.21"
+      source = "volterraedge/volterra"
+      version = "= 0.11.23"
     }
-
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = ">= 4.51.0"
     }
-
     azurerm = {
-      source  = "hashicorp/azurerm"
+      source = "hashicorp/azurerm"
       version = ">= 3.25.0"
     }
-
     google = {
-      source  = "hashicorp/google"
+      source = "hashicorp/google"
       version = ">= 4.48.0"
     }
-
     local = ">= 2.2.3"
-    null  = ">= 3.1.1"
+    null = ">= 3.1.1"
   }
 }
